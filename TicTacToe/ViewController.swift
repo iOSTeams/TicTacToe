22d867b85604dfc0bd9705efca11885c4d26ae73
Justin Lee <jlee402@mail.ccsf.edu>
2016-03-10 11:59:35 -0800

    Connected buttons to viewcontroller file, need to add whatever happens when pressed

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bottomRightButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var bottomLeftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var centerButton: UIButton!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var topRightButton: UIButton!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var topLeftButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func bottomRightClicked(sender: AnyObject) {
+    }
+    @IBAction func bottomClicked(sender: AnyObject) {
+    }
+    @IBAction func bottomLeftClicked(sender: AnyObject) {
+    }
+    @IBAction func rightClicked(sender: AnyObject) {
+    }
+    @IBAction func centerClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func topLeftClicked(sender: AnyObject) {
+    }
 
+    @IBAction func topClicked(sender: AnyObject) {
+    }
 
+    @IBAction func topRightClicked(sender: AnyObject) {
+    }
+    @IBAction func leftClicked(sender: AnyObject) {
+    }
 }
 
