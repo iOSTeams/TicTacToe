78a03ca99446a3943c57858d604d4906153d28bd
Justin Lee <jlee402@mail.ccsf.edu>
2016-03-13 12:00:42 -0700

    optimize

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     var counter = 0
     
+    @IBOutlet weak var firstScoreLabel: UILabel!
+    @IBOutlet weak var secondScoreLabel: UILabel!
     @IBOutlet weak var ResetButton: UIButton!
     @IBOutlet weak var bottomRightButton: UIButton!
     @IBOutlet weak var bottomButton: UIButton!
@@ -22,6 +24,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var topRightButton: UIButton!
     @IBOutlet weak var topButton: UIButton!
     @IBOutlet weak var topLeftButton: UIButton!
+    var firstScore = 0;
 
     
     override func viewDidLoad() {
@@ -34,6 +37,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func bottomRightClicked(sender: AnyObject) {
+        score()
         
         if (counter % 2 == 0) {
         bottomRightButton.setTitle("X", forState: .Normal)
@@ -41,10 +45,12 @@ class ViewController: UIViewController {
         }
         else {
             bottomRightButton.setTitle("O", forState: .Normal)
+            score()
             counter++
         }
     }
     @IBAction func bottomClicked(sender: AnyObject) {
+        score()
         if (counter % 2 == 0) {
             bottomButton.setTitle("X", forState: .Normal)
             counter++
@@ -55,32 +61,40 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func bottomLeftClicked(sender: AnyObject) {
+        score()
         if (counter % 2 == 0) {
             bottomLeftButton.setTitle("X", forState: .Normal)
             counter++
         }
         else {
+            score()
             bottomLeftButton.setTitle("O", forState: .Normal)
             counter++
         }
     }
     @IBAction func rightClicked(sender: AnyObject) {
+        score()
         if (counter % 2 == 0) {
             rightButton.setTitle("X", forState: .Normal)
+            score()
             counter++
         }
         else {
             rightButton.setTitle("O", forState: .Normal)
+            score()
             counter++
         }
     }
     @IBAction func centerClicked(sender: AnyObject) {
+
         if (counter % 2 == 0) {
             centerButton.setTitle("X", forState: .Normal)
+            score()
             counter++
         }
         else {
             centerButton.setTitle("O", forState: .Normal)
+            score()
             counter++
         }
     }
@@ -88,10 +102,12 @@ class ViewController: UIViewController {
     @IBAction func topLeftClicked(sender: AnyObject) {
         if (counter % 2 == 0) {
             topLeftButton.setTitle("X", forState: .Normal)
+            score()
             counter++
         }
         else {
             topLeftButton.setTitle("O", forState: .Normal)
+            score()
             counter++
         }
     }
@@ -99,10 +115,12 @@ class ViewController: UIViewController {
     @IBAction func topClicked(sender: AnyObject) {
         if (counter % 2 == 0) {
             topButton.setTitle("X", forState: .Normal)
+            score()
             counter++
         }
         else {
             topButton.setTitle("O", forState: .Normal)
+            score()
             counter++
         }
     }
@@ -110,20 +128,24 @@ class ViewController: UIViewController {
     @IBAction func topRightClicked(sender: AnyObject) {
         if (counter % 2 == 0) {
             topRightButton.setTitle("X", forState: .Normal)
+            score()
             counter++
         }
         else {
             topRightButton.setTitle("O", forState: .Normal)
+            score()
             counter++
         }
     }
     @IBAction func leftClicked(sender: AnyObject) {
         if (counter % 2 == 0) {
             leftButton.setTitle("X", forState: .Normal)
+            score()
             counter++
         }
         else {
             leftButton.setTitle("O", forState: .Normal)
+            score()
             counter++
         }
     }
@@ -141,5 +163,12 @@ class ViewController: UIViewController {
         
     }
     //2d ARRAY?
+    func score()    {
+        if (leftButton.titleLabel=="X" && centerButton.titleLabel=="X" && rightButton.titleLabel=="X"){
+            firstScore++
+            firstScoreLabel.text = String(firstScore)
+            
+        }
+    }
 }
 
